6da5bee08ef9349a702936ea7f56459250dda644
apoudel <apoudel@amd124.utah.cloudlab.us>
2025-05-11 16:02:06 +0000

    ensure mongo scales fix dns issue

--- socialNetwork/setup_social_network.sh
+++ socialNetwork/setup_social_network.sh
@@ -12,6 +12,8 @@ init_social_graph=false
 build_wrk2=false
 install_istio=false
 init_packages=false
+install_social_network=false
+uninstall_social_network=false
 
 for cmd in "$@"; do
     case $cmd in
@@ -19,6 +21,8 @@ for cmd in "$@"; do
         build-wrk2) build_wrk2=true ;;
         install-istio) install_istio=true ;;
         init-packages) init_packages=true ;;
+        install-social-network) install_social_network=true ;;
+        uninstall-social-network) uninstall_social_network=true ;;
         *) 
             mazu_echo "Unknown command: $cmd"
             ;;
@@ -67,4 +71,24 @@ fi
 if [[ "$init_social_graph" == "true" ]]; then
     mazu_echo "Initializing social graph..."
     python3 $SCRIPT_DIR/scripts/init_social_graph.py --graph=socfb-Reed98
+fi
+
+if [[ "$install_social_network" == "true" ]]; then
+    mazu_echo "Upgrading social network..."
+    helm upgrade --install social-network $SCRIPT_DIR/helm-chart/socialnetwork/ \
+        --set global.mongodb.sharding.enabled=true,global.mongodb.standalone.enabled=false \
+        --timeout 10m0s --wait
+
+#         --set global.memcached.cluster.enabled=true,global.memcached.standalone.enabled=false \
+#         --set global.redis.replication.enabled=true,global.redis.standalone.enabled=false \
+#         --set global.redis.cluster.enabled=true,global.redis.standalone.enabled=false \
+fi
+
+if [[ "$uninstall_social_network" == "true" ]]; then
+    mazu_echo "Uninstalling social network..."
+    helm uninstall social-network
+
+    # remove pvc
+    for p in $(kubectl get pvc -o name -l app.kubernetes.io/name=mongodb-sharded); do kubectl delete $p; done
+    for p in $(kubectl get pvc -o name -l app.kubernetes.io/name=redis-cluster); do kubectl delete $p; done
 fi
