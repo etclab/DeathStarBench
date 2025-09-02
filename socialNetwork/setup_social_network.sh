606fa7f4bbe5d965f0a42e84fd8c96ecc47149a0
apoudel <apoudel@c220g1-031103.wisc.cloudlab.us>
2025-09-01 22:50:07 -0500

    use bookinfo for debug

--- socialNetwork/setup_social_network.sh
+++ socialNetwork/setup_social_network.sh
@@ -30,6 +30,9 @@ for cmd in "$@"; do
         build-wrk2) build_wrk2=true ;;
         install-istio) install_istio=true ;;
         install-mazu) install_mazu=true ;;
+        install-bf) install_bf=true ;;
+        get-ingress) get_ingress=true ;;
+        uninstall-bf) uninstall_bf=true ;;
         init-packages) init_packages=true ;;
         install-social-network) install_social_network=true ;;
         uninstall-social-network) uninstall_social_network=true ;;
@@ -67,7 +70,8 @@ fi
 
 if [[ "$install_mazu" == "true" ]]; then
     DOCKER_HUB=docker.io/atosh502 
-    DOCKER_TAG=st3-TokRev
+    # DOCKER_TAG=st3-TokRev
+    DOCKER_TAG=st2-NIChaRes
 
     if [ ! -x "$ISTIOCTL_PATH" ]; then
         mazu_echo "Installing istioctl..."
@@ -81,7 +85,8 @@ if [[ "$install_mazu" == "true" ]]; then
     "$SCRIPT_DIR/dev/install_etcd.sh"
 
     "$ISTIOCTL_PATH" install --set profile=default --set hub=$DOCKER_HUB \
-        --set tag=$DOCKER_TAG --set "values.global.imagePullPolicy=Always" -y
+        --set tag=$DOCKER_TAG --set "values.global.imagePullPolicy=Always" -y \
+        --set values.global.proxy.resources.limits.memory=2Gi \
 
     kubectl apply -f "$SCRIPT_DIR/dev/token-review-role.yaml" 
     kubectl apply -f "$SCRIPT_DIR/dev/token-review-binding.yaml"
@@ -122,14 +127,27 @@ if [[ "$init_social_graph" == "true" ]]; then
 fi
 
 if [[ "$install_social_network" == "true" ]]; then
+    mazu_echo "Configuring gateway and virtual service..."
+    kubectl apply -f $SCRIPT_DIR/kubernetes/istio-gateway.yaml
+    
     mazu_echo "Upgrading social network..."
     kubectl apply -f $SCRIPT_DIR/scratch/yaml/mcrouter-role.yaml
 
     helm upgrade --install social-network $SCRIPT_DIR/helm-chart/socialnetwork/ \
         --timeout 10m0s --wait
 
-    mazu_echo "Configuring gateway and virtual service..."
-    kubectl apply -f $SCRIPT_DIR/kubernetes/istio-gateway.yaml
+fi
+
+if [[ "$install_bf" == "true" ]]; then
+    mazu_echo "Installing Bookinfo application..."
+    kubectl apply -f $SCRIPT_DIR/scratch/yaml/bookinfo.yaml
+    kubectl apply -f $SCRIPT_DIR/scratch/yaml/bf-gateway.yaml
+fi
+
+if [[ "$uninstall_bf" == "true" ]]; then
+    mazu_echo "Uninstalling Bookinfo application..."
+    kubectl delete -f $SCRIPT_DIR/scratch/yaml/bookinfo.yaml
+    kubectl delete -f $SCRIPT_DIR/scratch/yaml/bf-gateway.yaml
 fi
 
 if [[ "$uninstall_social_network" == "true" ]]; then
@@ -158,19 +176,29 @@ if [[ "$uninstall_social_network" == "true" ]]; then
     kubectl delete -f $SCRIPT_DIR/kubernetes/istio-gateway.yaml
 fi
 
+if [[ "$get_ingress" == "true" ]]; then
+    # fetch ingress IP and port
+    get_ingress_ip_port
+
+    echo "Ingress IP: $INGRESS_IP"
+    echo "Ingress Port: $INGRESS_PORT"
+fi
+
 if [[ "$run_mixed_load" == "true" ]]; then
     mazu_echo "Running mixed workload..."
 
     # fetch ingress IP and port
     get_ingress_ip_port
 
-    mkdir -p results/mixed
+    # mkdir -p results/mixed
+    mkdir -p results/home
 
     # for reqs in 1000 2000 3000 4000
-    for reqs in 100 
+    for reqs in 1 
     do
         echo "Running for ${reqs} reqs"
-        ../wrk2/wrk -D exp -t 10 -c 10 -d 60 -L -s ./wrk2/scripts/social-network/mixed-workload.lua http://$INGRESS_IP:$INGRESS_PORT -R ${reqs} >> results/mixed/${reqs}.txt
+        # ../wrk2/wrk -D exp -t 1 -c 1 -d 1 -L -s ./wrk2/scripts/social-network/mixed-workload.lua http://$INGRESS_IP:$INGRESS_PORT -R ${reqs} >> results/mixed/${reqs}.txt
+        ../wrk2/wrk -D exp -t 1 -c 1 -d 1 -L -s ./wrk2/scripts/social-network/read-home-timeline.lua http://$INGRESS_IP:$INGRESS_PORT -R ${reqs} >> results/home/${reqs}.txt
     done
     echo "=== All tests completed ==="
     echo "Results saved in results/mixed/"
--- socialNetwork/wrk2/scripts/social-network/read-home-timeline.lua
+++ socialNetwork/wrk2/scripts/social-network/read-home-timeline.lua
@@ -15,7 +15,23 @@ request = function()
   local method = "GET"
   local headers = {}
   headers["Content-Type"] = "application/x-www-form-urlencoded"
-  local path = "http://localhost:8080/wrk2-api/home-timeline/read?" .. args
+  -- local path = "http://localhost:8080/wrk2-api/home-timeline/read?" .. args
+  local path = "/wrk2-api/home-timeline/read?" .. args
   return wrk.format(method, path, headers, nil)
 
+end
+
+-- remove this to disable response logging
+response = function(status, headers, body)
+    -- Log the status code
+    io.stderr:write("Status: " .. status .. "\n")
+
+    -- Log specific headers (e.g., Content-Type)
+    if headers["Content-Type"] then
+        io.stderr:write("Content-Type: " .. headers["Content-Type"] .. "\n")
+    end
+
+    -- Log the response body (be cautious with large bodies)
+    -- You might want to truncate or only log if specific conditions are met
+    io.stderr:write("Body: " .. body .. "\n")
 end
